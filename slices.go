cb4dbf3506bb0bea8bf1318d397b014847d857bf
Vaibhav Vijay Nayak <vaibhavn993@gmail.com>
2026-01-02 10:21:13 +0530

    Use equal method in slices

--- slices.go
+++ slices.go
@@ -1,24 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 func main() {
-	var nums []int;
-	fmt.Println(nums);
+	var nums []int
+	fmt.Println(nums)
 	nums = append(nums, 1)
-	fmt.Println(nums);
+	fmt.Println(nums)
 
-	stringArray := make([]string, 3);
-	fmt.Println(stringArray);
+	stringArray := make([]string, 3)
+	fmt.Println(stringArray)
 	stringArray[0] = "123"
 	stringArray[1] = "ABC"
 	stringArray[2] = "9090"
-	fmt.Println(stringArray);
+	fmt.Println(stringArray)
 	fmt.Println("Len is ", len(stringArray))
 
 	stringArray = append(stringArray, "appended_element")
 	fmt.Println(stringArray, len(stringArray))
 
 	fmt.Println(stringArray[:2])
+
+	if slices.Equal(nums, []int{1}) {
+		fmt.Println("They are equal");
+	}
 
 }
